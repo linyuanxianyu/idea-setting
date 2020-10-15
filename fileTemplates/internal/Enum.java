881137479a866150298e261dfbef14e7ac56fe9f
zyb <zyb@test.com>
2020-10-15 10:21:07 +0800

    IU-2019.3.1 <zyb@LAPTOP-2VPJTD7T Create baseRefactoring.xml, markdown.xml, ide.general.xml, project.default.xml, diff.xml, Enum.java, Default copy.xml, Interface.java, yiiguxing.translation.xml, terminal.xml, Default.xml, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, keymap.xml, javaeeExternalResources.xml, colors.scheme.xml, MyKeys.xml, databaseDrivers.xml, github.xml, mavenVersion.xml, templates.xml, Default.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, Jsp File.jsp, wsl.distributions.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+* @description: ${description}
+* @author: zyb
+* @create: ${YEAR}-${MONTH}-${DAY}
+**/
+public enum ${NAME} {
+}
