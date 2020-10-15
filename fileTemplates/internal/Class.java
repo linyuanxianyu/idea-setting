18e34dafa619f2f103a04cd2959c10d2b6bd5df9
zyb <zyb@test.com>
2020-10-15 11:01:49 +0800

    IU-2019.3.1 <zyb@LAPTOP-2VPJTD7T Overwrite remote https://github.com/linyuanxianyu/idea-setting.git to local

--- fileTemplates/internal/Class.java
+++
@@ -1,9 +0,0 @@
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-#parse("File Header.java")
-/**
-* @description: ${description}
-* @author: zyb
-* @create: ${YEAR}-${MONTH}-${DAY}
-**/
-public class ${NAME} {
-}
